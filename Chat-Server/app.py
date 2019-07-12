e27ec0a831e5dcdd7b46c4611df826fc170be208
Morstern <kamilziel@tlen.pl>
2019-07-12 19:42:28 +0200

    Add basic structure of Chat-server
    Add app.py - file which contains all server logic

---
+++ Chat-Server/app.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+@socketio.on('connect')
+def test_connect():
+    emit('after connect', {'data': "Let's dance"})
+
+@socketio.on('message')
+def resend_message(message):
+    emit('update chat', message['data'], broadcast=True)
+
+if __name__ == '__main__':
+    socketio.run(app)
