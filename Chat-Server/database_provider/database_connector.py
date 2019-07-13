2b9c852ba941ee239e813eb123ff9b84077c6ccb
Morstern <kamilziel@tlen.pl>
2019-07-13 09:36:56 +0200

    Fixed issue with getting values from dictionaries

--- Chat-Server/database_provider/database_connector.py
+++ Chat-Server/database_provider/database_connector.py
@@ -1,32 +1,29 @@
 from pymongo import MongoClient
-from bson.objectid import ObjectId
 
 
 class MongoDBConnector():
     def __init__(self, collection_name):
         """ Generic MongoDB class
-
         self.collection -> collection from which we fetch data and upload to it
         """
         client = MongoClient('localhost', 27017)
         db = client['Messenger']
-        self.collection = db.converastions
+        self.collection = db[collection_name]
         self.array = 'messages'
 
     def add_message_to_conversation(self, data):
         """Add message to existing conversation
-
         We find the conversation, and push a new message
         to its array 'messages'
         """
         self.collection.find_one_and_update(
-                {'_id': ObjectId(data.id_postu)},
+                {'conversation_id': data['conversation_id']},
                 {
                     '$push': {
                         self.array: {
-                            'author': data.author,
-                            'text': data.text,
-                            'date': data.date
+                            'author': data['author'],
+                            'text': data['text'],
+                            'date': data['date']
                         }
                     }
                 }
@@ -35,16 +32,16 @@ class MongoDBConnector():
     def create_conversation(self, data):
         """ Create conversation for users """
         conversation = {
-            'conversation_id': data.conversation_id,
+            'conversation_id': data['conversation_id'],
             'messages': []
         }
 
-        self.collection.insert_one(conversation)
+        return self.collection.insert_one(conversation)
 
     def fetch_conversation(self, data):
         """ Fetching all messages from conversation """
         conversation = self.collection.find_one(
-            {"conversation_id": data.conversation_id}
+            {"conversation_id": data['conversation_id']}
         )
 
         return conversation
