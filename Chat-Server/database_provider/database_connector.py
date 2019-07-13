385cdda402b1db23bb52ce40a39d5069da7e7ce8
Morstern <kamilziel@tlen.pl>
2019-07-13 08:57:10 +0200

    Add class which helps with DB functionality
    Fixed requirements.txt, now it has libraries dedicated to this project
    Deleted .gitkeep

---
+++ Chat-Server/database_provider/database_connector.py
@@ -0,0 +1,50 @@
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+
+class MongoDBConnector():
+    def __init__(self, collection_name):
+        """ Generic MongoDB class
+
+        self.collection -> collection from which we fetch data and upload to it
+        """
+        client = MongoClient('localhost', 27017)
+        db = client['Messenger']
+        self.collection = db.converastions
+        self.array = 'messages'
+
+    def add_message_to_conversation(self, data):
+        """Add message to existing conversation
+
+        We find the conversation, and push a new message
+        to its array 'messages'
+        """
+        self.collection.find_one_and_update(
+                {'_id': ObjectId(data.id_postu)},
+                {
+                    '$push': {
+                        self.array: {
+                            'author': data.author,
+                            'text': data.text,
+                            'date': data.date
+                        }
+                    }
+                }
+        )
+
+    def create_conversation(self, data):
+        """ Create conversation for users """
+        conversation = {
+            'conversation_id': data.conversation_id,
+            'messages': []
+        }
+
+        self.collection.insert_one(conversation)
+
+    def fetch_conversation(self, data):
+        """ Fetching all messages from conversation """
+        conversation = self.collection.find_one(
+            {"conversation_id": data.conversation_id}
+        )
+
+        return conversation
