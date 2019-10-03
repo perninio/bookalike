5908049d48e354372686c0b56227876bd725a793
Morstern <kamilziel@tlen.pl>
2019-10-03 17:30:09 +0200

    Create Navbar

---
+++ Web-Server/frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Navbar } from "./components/common/Navbar";
+import { Footer } from "./components/common/Footer";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
