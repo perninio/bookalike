827f32575a07aa6bbeb8bb0657dfb2f04d748ec3
boromir <przemekshepard@gmail,com>
2019-10-20 13:05:25 +0200

    created book page layout and temporary modified index.html css style for planning website views

--- Web-Server/frontend/src/components/layout/public/book/BookPage.js
+++ Web-Server/frontend/src/components/layout/public/book/BookPage.js
@@ -21,5 +21,62 @@ export const BookPage = props => {
 
   console.log(data);
 
-  return <React.Fragment></React.Fragment>;
+  return <React.Fragment>
+        
+<div class="container">
+  <div class="row my-row">
+      
+    <div class="col-md=auto my-col order-sm-1">
+            <span class='bookcover'>
+
+      <img  src="https://skupszop.pl/images/books/9788375745368.jpg" alt='Pan lodowego ogrodu'>
+    </img>
+
+</span>
+    </div>
+
+    <div class="col-md my-col  order-sm-2">
+        <span class="bookinfo">
+                <h2 >
+                        Pan lodowego ogrodu
+                    </h2>
+                    <div>
+                    Ocena: 4,79/5
+                    </div>
+    <div>
+    autor: Grzędowicz
+    </div>
+      Opis: Dawno dawno temu, za siedmioma górami ....
+      </span>
+    </div>
+    <div class="col-md my-col order-md-3 order-sm-4">
+     reklamy kiedyś może google albo/rtbkit
+    </div>
+
+    <div  class="col-md-12 my-col order-md-4 order-sm-3">
+            <span class="stars">
+               gwiazdki
+              
+               </span>
+        </div>
+  </div>
+
+ <div class="row justify-content-md-center my-row">
+        <div class="col-md-12 my-col text-center">
+               Carouzela z rekomndacjami
+               </div>
+      </div>
+
+
+    <div class="row my-row-comment">
+            <div  class="col-md-12 my-col text-center">
+                <span class="comment">
+                    <img src="https://www.tubefilter.com/wp-content/uploads/2013/11/new-youtube-comments-rosanna-pansino.jpg"/>
+                
+                </span>
+            </div>
+    </div>
+</div>
+  
+  </React.Fragment>;
 };
